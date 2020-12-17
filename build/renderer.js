4fc6d79cd9ebe702c4955353d244c324fb961e20
Kyle Andrews <codingwithkyle@gmail.com>
2020-12-16 20:43:53 -0500

    Removed JSDOM & created custom data injection logic

--- build/renderer.js
+++ build/renderer.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const cwd = process.cwd();
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
 const base = path.join(cwd, "src", "base.html");
 if (!fs.existsSync(base)){
@@ -11,10 +9,7 @@ if (!fs.existsSync(base)){
 }
 
 let html = fs.readFileSync(base).toString();
-let dom = new JSDOM(html);
-dom = resolveIncludes(dom.window.document.documentElement, path.join(cwd, "src"));
-removeIncludes(dom);
-html = dom.outerHTML;
+html = resolveIncludes(html, path.join(cwd, "src"));
 
 const public = path.join(cwd, "public", "index.html");
 if (fs.existsSync(public)){
@@ -23,41 +18,46 @@ if (fs.existsSync(public)){
 fs.writeFileSync(public, html);
 process.exit(0);
 
-function removeIncludes(element){
-    const elements = Array.from(element.querySelectorAll("include"));
-    for (let i = 0; i < elements.length; i++){
-        elements[i].remove();
-    }
-}
-
-function resolveIncludes(element, activeDir){
-    const includeElements = Array.from(element.querySelectorAll("include"));
-    for (let i = 0; i < includeElements.length; i++){
-        let localPath = path.resolve(activeDir, includeElements[i].getAttribute("src"));
-        if (localPath){
-            if (new RegExp(/(\.html)$/).test(localPath)){
-                localPath = path.resolve(activeDir, localPath);
-            } else {
-                localPath = path.resolve(activeDir, localPath, "index.html");
-            }
-            if (fs.existsSync(localPath)){
-                const newActivePath = path.resolve(activeDir, `${localPath.replace(/(?!.*[\/\\]).*/, "")}`);
-                let html = fs.readFileSync(localPath).toString();
-                for (const key in includeElements[i].dataset){
-                    const regex = new RegExp(`\\{[\\s+]?${key}[\\s+]?\\}`, "g");
-                    html = html.replace(regex, includeElements[i].dataset[key]);
-                }
-                const tempDOM = new JSDOM(html);
-                const newElements = Array.from(tempDOM.window.document.documentElement.querySelectorAll("body > *"));
-                for (let i = 0; i < newElements.length; i++){
-
-                    const newElement = resolveIncludes(newElements[i], newActivePath);
-                    includeElements[i].insertAdjacentElement("beforebegin", newElement);
-                }
-            }
-        } else {
-            includeElements[i].insertAdjacentElement("beforebegin", `<p class="block w-full text-center font-danger-700">Missing ${localPath}</p>`);
+function parseDataset(html){
+    const dataset = {};
+    const attributes = html.match(/data\-.*?[\'\"].*?[\'\"]/g);
+    if (attributes?.length){
+        for (let i = 0; i < attributes.length; i++){
+            const key = attributes[i].replace(/(data\-)|\=.*/g, "");
+            const value = attributes[i].replace(/(data\-.*?[\'\"])|[\'\"]/g, "");
+            dataset[key] = value;
         }
     }
-    return element;
+    return dataset;
+}
+
+function resolveIncludes(html, activeDir){
+    html = html.replace(/\<\/([\s]+)?include([\s]+)?\>/g, "");
+    const includeElements = html.match(/\<include.*?\>/g);
+    let output = html;
+    if (includeElements?.length){
+        for (let i = 0; i < includeElements.length; i++){
+            let localPath = includeElements[i].replace(/(\<include.*src\=[\'\"])|[\'\"].*/g, "");
+            let snippetHTML = `<p class="font-danger-700">Missing file: '${localPath}'</p>`;
+            if (localPath){
+                if (new RegExp(/(\.html)$/).test(localPath)){
+                    localPath = path.resolve(activeDir, localPath);
+                } else {
+                    localPath = path.resolve(activeDir, localPath, "index.html");
+                }
+                if (fs.existsSync(localPath)){
+                    const newActivePath = path.resolve(activeDir, `${localPath.replace(/(?!.*[\/\\]).*/, "")}`);
+                    let newHTML = fs.readFileSync(localPath).toString();
+                    const dataset = parseDataset(html);
+                    for (const key in dataset){
+                        const regex = new RegExp(`\\{[\\s+]?${key}[\\s+]?\\}`, "g");
+                        newHTML = newHTML.replace(regex, dataset[key]);
+                    }
+                    snippetHTML = resolveIncludes(newHTML, newActivePath);
+                }
+            }
+            output = output.replace(includeElements[i], snippetHTML);
+        }
+    }
+    return output;
 }
--- src/views/editor-drawer/sponsor-settings/sponsor-settings.ts
+++ src/views/editor-drawer/sponsor-settings/sponsor-settings.ts
@@ -3,6 +3,7 @@ import { Component } from "djinnjs/component";
 import { fetchCSS } from "djinnjs/fetch";
 import { html, render } from "lit-html";
 import { mount } from "utils/mount";
+import { refresh } from "tooltipper";
 
 import { SponsorUploadButton } from "./sponsor-upload-button/sponsor-upload-button";
 mount("sponsor-upload-button", SponsorUploadButton);
@@ -51,7 +52,7 @@ export default class SponsorSettings extends Component<State>{
             view = html`
                 <sponsor-upload-button class="mb-1">
                     <input id="sponsor-upload" type="file" multiple accept="image/*">
-                    <label for="sponsor-upload" class="button -solid -primary -icon w-full">
+                    <label for="sponsor-upload" class="button -solid -primary -icon w-full -rounded">
                         <i class="icon">
                             <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm65.18 216.01H224v80c0 8.84-7.16 16-16 16h-32c-8.84 0-16-7.16-16-16v-80H94.82c-14.28 0-21.41-17.29-11.27-27.36l96.42-95.7c6.65-6.61 17.39-6.61 24.04 0l96.42 95.7c10.15 10.07 3.03 27.36-11.25 27.36zM377 105L279.1 7c-4.5-4.5-10.6-7-17-7H256v128h128v-6.1c0-6.3-2.5-12.4-7-16.9z"></path></svg>
                         </i>
@@ -65,9 +66,9 @@ export default class SponsorSettings extends Component<State>{
                 <copy-editor loading="eager" class="mb-2" data-lookup="js-sponsor-label">
                     <textarea-component web-component css="textarea-component">
                         <label for="sponsor-label-textarea-component" class="block w-full mb-1">Sponsor Label</label>
-                        <textarea id="sponsor-label-textarea-component" rows="5" style="margin-right: 36px;width: calc(100% - 36px);border-radius: 0.25rem 0 0 0.25rem;">Sponsored by</textarea>
+                        <textarea id="sponsor-label-textarea-component" rows="5" style="border-radius: 0 0 0.25rem 0.25rem;">Sponsored by</textarea>
                     </textarea-component>
-                    <copy-editor-actions>
+                    <copy-editor-actions style="top: 2rem;">
                         <button state="hidden" class="js-toggle-visiblity-button visibility" aria-label="enable heading" tooltip="Enable">
                             <svg class="hidden" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z"></path></svg>
                             <svg class="visible" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"></path></svg>
@@ -93,5 +94,8 @@ export default class SponsorSettings extends Component<State>{
             `;
         }
         render(view, this.container);
+        if (this.state.visible){
+            refresh();
+        }
     }
 }
