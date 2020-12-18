0c606ef23caad466f7a6e3d171f75ea7d2476038
Kyle Andrews <codingwithkyle@gmail.com>
2020-12-17 19:49:28 -0500

    Fixed service worker dynamic caching logic

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,18 +25,32 @@ async function onActivate(event) {
 }
 
 async function onFetch(event) {
-    let cachedResponse = null;
-    if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate';
+    if (event.request.method !== "GET") {
+        // Use a passthrough to ignore the caching system
+        return fetch(event.request);
+    } else {
+        return caches.match(event.request).then(response => {
+            if (response) {
+                return response;
+            }
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
-        const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
+            return fetch(event.request, {
+                redirect: "follow",
+            }).then(response => {
+                if (!response || response.status !== 200 || response.type !== "basic" || response.redirected) {
+                    return response;
+                }
+
+                var responseToCache = response.clone();
+
+                caches.open(cacheName).then(cache => {
+                    cache.put(event.request, responseToCache);
+                });
+                
+                return response;
+            });
+        });
     }
-
-    return cachedResponse || fetch(event.request);
 }
 
 async function cachebust(){
